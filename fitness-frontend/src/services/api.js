71caeb4870b28224a2e2fd061b10965505bcac2f
dheeraj <dheeraj@Mac-1054.lan>
2025-12-18 22:02:43 -0800

    fitness-6

--- fitness-frontend/src/services/api.js
+++ fitness-frontend/src/services/api.js
@@ -31,7 +31,7 @@
 // api.js
 import axios from "axios";
 
-const API_URL = 'http://localhost:8080/api';
+const API_URL = `${process.env.REACT_APP_API_BASE_URL}`;
 
 const api = axios.create({
   baseURL: API_URL,
